86d549f9e168322f47590252a0a77d17760f3393
ahrorbeksulaymanov <78092308+ahrorbeksulaymanov@users.noreply.github.com>
2022-02-02 21:18:51 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import './App.css';
 import MainPage from './components/main/main';
 
 function App() {
-
-  // const [id, setId] = useState(undefined);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("https://front-test.beta.aviasales.ru/search")
-  //     .then((res) => setId(res?.data?.searchId));
-  //   }, []);
-
 
   return (
     <div className="App">
